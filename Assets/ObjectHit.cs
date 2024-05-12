dd45ef1b27948c6aa2a178cf462a995b17ae7521
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-12 11:30:39 +0100

    GetComponent example added

--- Assets/ObjectHit.cs
+++ Assets/ObjectHit.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    MeshRenderer objectRenderer;
+
+    private void Awake() {
+        objectRenderer = GetComponent<MeshRenderer>();
+    }
     private void OnCollisionEnter(Collision other) {
         Debug.Log($"{other} bumped into the wall");
+        objectRenderer.material.color = Color.red;
     }
 }
